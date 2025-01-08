ef538868b6af85f25aad53177b9b5d0418892f99
wrong <wenlong.ruan@qq.com>
2025-01-08 11:04:26 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// TODO validate contact form fields
+const inputs = document.querySelectorAll(".form-group input");
+const textarea = document.querySelector("textarea");
+const btn = document.querySelector(".submit-btn");
+
+inputs.forEach((input) => {
+  addEventListener(input);
+});
+addEventListener(textarea);
+
+function addEventListener(element) {
+  element.addEventListener("focus", () => {
+    element.parentElement.classList.add("actived");
+  });
+
+  element.addEventListener("blur", () => {
+    element.parentElement.classList.remove("actived");
+  });
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputs.forEach((input) => {
+    console.log(input);
+  });
+});
